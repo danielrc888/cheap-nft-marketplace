c13b3d88a899927a38e26d1a8ceb5bc0d9319609
Daniel R <danielrojasc888@gmail.com>
2025-01-01 19:48:14 -0500

    add auction service

---
+++ backend/src/services/auctionService.ts
@@ -0,0 +1,32 @@
+import { AuctionModel } from "../models/auctionModel";
+import { Auction, Bid } from "../types/auctionTypes";
+
+const auctionModel = new AuctionModel();
+
+export const createAuction = (nftAddress: string, owner: string, minPrice: number, erc20Address: string): Auction => {
+    return auctionModel.createAuction(nftAddress, owner, minPrice, erc20Address);
+};
+
+export const placeBid = (auctionId: string, bidder: string, amount: number, signature: string): Bid | null => {
+    return auctionModel.createBid(auctionId, bidder, amount, signature);
+};
+
+export const approveBid = (auctionId: string, bidId: string): Auction | null => {
+    return auctionModel.approveBid(auctionId, bidId);
+};
+
+export const getAuctionDetails = (auctionId: string) => {
+    return auctionModel.getAuction(auctionId);
+};
+
+export const getAllAuctions = () => {
+    return auctionModel.getAllAuctions();
+};
+
+export const getAllBids = (auctionId: string) => {
+    return auctionModel.getBids(auctionId);
+};
+
+export const getBidDetails = (auctionId: string, bidId: string) => {
+    return auctionModel.getBid(auctionId, bidId);
+}
